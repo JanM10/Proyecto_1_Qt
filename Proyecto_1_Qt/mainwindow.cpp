31d15b66dc3a31f953ab653657c113ba85db8978
JanM10 <JanM10@users.noreply.github.com>
2021-04-06 21:00:06 -0600

    Se crea una ventana con botones, labels y entradas de textos. Esto va a ser la base para el IDE de C!

--- Proyecto_1_Qt/mainwindow.cpp
+++ Proyecto_1_Qt/mainwindow.cpp
@@ -13,3 +13,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_clearButton_clicked()
+{
+    ui->aplicationLogConsole->clear();
+}
--- Proyecto_1_Qt/mainwindow.h
+++ Proyecto_1_Qt/mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_clearButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
