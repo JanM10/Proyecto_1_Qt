5a8b3357e32092e4c6a1fcaec378a2a9ac223116
JanM10 <JanM10@users.noreply.github.com>
2021-04-12 22:27:13 -0600

    Se implementa una funciona para detectar cuando hay un salto de linea dentro de un textedit.

--- Proyecto_1_Qt/mainwindow.cpp
+++ Proyecto_1_Qt/mainwindow.cpp
@@ -3,8 +3,12 @@
 #include "localserver.h"
 #include <QMessageBox>
 #include <iostream>
+#include <string>
+#include <sstream>
 
-QString variable = "";
+using namespace std;
+
+QStringList lines;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -43,6 +47,21 @@ void MainWindow::on_runButton_clicked()
 
 void MainWindow::on_writeCode_textChanged()
 {
-    ui->console->setText(ui->writeCode->toPlainText());
+//    ui->console->setText(ui->writeCode->toPlainText());
     //std::cout << "Text: " << ui->writeCode->toPlainText().toStdString() << std::endl;
+
+    if(ui->writeCode->toPlainText().contains("\n")){
+        lines = ui->writeCode->toPlainText().split("\n");
+//        cout<<lines.size()<<endl;
+    }
+
+    QString texto = "";
+    for(int i=0; i<lines.size()-1; i++){
+        texto += lines[i] + "\n";
+    }
+    ui->console->setText(texto);
+    lines.clear();
 }
+
+//    ui->console->setText(lines.count());
+//    QStringList lines = ui->writeCode->toPlainText().split("\n");
